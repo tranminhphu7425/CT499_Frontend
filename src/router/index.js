806500e05282015a2dd095830fd1108070ba9383
tranminhphu7425 <90190034+tranminhphu7425@users.noreply.github.com>
2026-09-15 21:12:11 +0700

    Cap nhat them chuc nang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,21 @@ const routes = [
     component: ContactBook,
   },
   {
-    path: "/:pathMatch(.*)*",
-    name: "notfound",
-    component: () => import("@/views/NotFound.vue"),
+    path: "/contacts/add",
+    alias: "/contacts",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
   },
   {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true
+    props: true,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
   },
 ];
 
@@ -26,5 +32,3 @@ const router = createRouter({
 });
 
 export default router;
-
-
