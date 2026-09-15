50728297634a329e546c848916d332bf9e9a253e
tranminhphu7425 <90190034+tranminhphu7425@users.noreply.github.com>
2026-09-15 18:08:42 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/services/api.service.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const commonConfig = {
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+};
+
+export default (baseURL) => {
+    return axios.create({
+        baseURL: baseURL,
+        ...commonConfig,
+    });
+};
+
+
+
+
